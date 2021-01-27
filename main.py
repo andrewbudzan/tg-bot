3c2b2f5e6ea1b20509f0d27312193de2ab4a878e
andriibudzan <andrewbudzan@gmail.com>
2021-01-27 05:28:12 +0200

    fix

--- main.py
+++ main.py
@@ -27,26 +27,26 @@ def send_welcome(message):
     bot.reply_to(message, f"Sorry, {message.from_user.first_name}, I can't help you at the moment...")
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_message(message):
-    text = message.text.lower()
-    user = message.from_user
-    if text == 'hello':
-        bot.send_message(user.id, f'Hello {user.first_name}!')
-    elif text == 'time':
-        bot.send_message(user.id, f'{time.strftime("%H:%M:%S", time.gmtime())}')
-    elif text == 'my_id':
-        bot.send_message(user.id, f'{user.id}')
-    elif text == 'my_data':
-        bot.send_message(user.id, f'{user.__dict__}')
-    elif text == 'date':
-        bot.send_message(user.id, f'{time.strftime("%A, %d %B %Y", time.gmtime())}')
-    elif text == 'datetime':
-        bot.send_message(user.id, f'{time.strftime("%A, %d %B %Y %H:%M:%S +0000", time.gmtime())}')
-    elif text == 'date_full':
-        bot.send_message(user.id, f'{time.strftime("%c", time.gmtime())}')
-    else:
-        bot.send_message(user.id, f"Sorry, I don't understand you!")
+# @bot.message_handler(content_types=['text'])
+# def get_text_message(message):
+#     text = message.text.lower()
+#     user = message.from_user
+#     if text == 'hello':
+#         bot.send_message(user.id, f'Hello {user.first_name}!')
+#     elif text == 'time':
+#         bot.send_message(user.id, f'{time.strftime("%H:%M:%S", time.gmtime())}')
+#     elif text == 'my_id':
+#         bot.send_message(user.id, f'{user.id}')
+#     elif text == 'my_data':
+#         bot.send_message(user.id, f'{user.__dict__}')
+#     elif text == 'date':
+#         bot.send_message(user.id, f'{time.strftime("%A, %d %B %Y", time.gmtime())}')
+#     elif text == 'datetime':
+#         bot.send_message(user.id, f'{time.strftime("%A, %d %B %Y %H:%M:%S +0000", time.gmtime())}')
+#     elif text == 'date_full':
+#         bot.send_message(user.id, f'{time.strftime("%c", time.gmtime())}')
+#     else:
+#         bot.send_message(user.id, f"Sorry, I don't understand you!")
 
 
 def start_bot():
